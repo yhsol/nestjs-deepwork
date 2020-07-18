c40477c343fbe0b3b9cdf9d0843f11f1042561e3
loshy <loshy244110@gmail.com>
2020-07-18 20:24:02 +0900

    feat: connect database with postgresql

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,11 +4,11 @@ dotenv.config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT, 10),
+  username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
-  database: 'deepwork',
+  database: process.env.DATABASE_NAME,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
