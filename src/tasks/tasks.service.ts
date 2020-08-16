f7189f17b639d505b947269ebb98478d0fe8de53
loshy <loshy244110@gmail.com>
2020-08-16 16:57:27 +0900

    feat: create task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { v1 as uuidv1 } from 'uuid';
 
 @Injectable()
 export class TasksService {
@@ -9,5 +10,22 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  createTask(title: string, desc: string): Task {
+    // parameter 를 활용해서 task 구성
+    const task: Task = {
+      id: uuidv1(),
+      title,
+      desc,
+      status: TaskStatus.OPEN,
+    };
+
+    // 생성한 task 를 tasks 에 push
+    this.tasks.push(task);
+    // task 에 추가 작업을 한 뒤에,
+    // 생성한 task 를 리턴
+    // 생성한 task 를 리턴함으로써 다시 한번 생성 된 task 를 확인할 수 있음.
+    return task;
   }
 }
